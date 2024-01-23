92ab878666decce44ab6bed9847068aeba40b484
aitseb <lchlebda@gmail.com>
2024-01-23 17:50:58 +0100

    Aktualizacja main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,14 +9,14 @@ makerbit.onIrButton(IrButton.Number_8, IrButtonAction.Pressed, function () {
 makerbit.onIrButton(IrButton.Number_0, IrButtonAction.Pressed, function () {
     isGoingForward = false
     maqueenPlusV2.showColor(maqueenPlusV2.NeoPixelColors.Red)
-    maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Backward, 150)
+    maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Backward, speed)
     basic.showArrow(ArrowNames.South)
     basic.clearScreen()
 })
 makerbit.onIrButton(IrButton.Number_5, IrButtonAction.Pressed, function () {
     isGoingForward = true
     maqueenPlusV2.showColor(maqueenPlusV2.NeoPixelColors.Green)
-    maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Forward, 150)
+    maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Forward, speed)
     basic.showArrow(ArrowNames.North)
     basic.clearScreen()
 })
@@ -30,6 +30,27 @@ makerbit.onIrButton(IrButton.Number_7, IrButtonAction.Pressed, function () {
     maqueenPlusV2.controlMotorStop(maqueenPlusV2.MyEnumMotor.AllMotor)
     maqueenPlusV2.ledBlank()
 })
+makerbit.onIrButton(IrButton.Down, IrButtonAction.Pressed, function () {
+    if (speed <= 235) {
+        speed += 20
+    }
+    music.play(music.tonePlayable(587, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    basic.showNumber(speed)
+})
+makerbit.onIrDatagram(function () {
+    if (makerbit.irButton() == 224) {
+        if (speed >= 20) {
+            speed += -20
+        }
+        music.play(music.tonePlayable(294, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        basic.showNumber(speed)
+    }
+    if (makerbit.irButton() == 144) {
+        speed = 255
+        music.play(music.tonePlayable(294, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        basic.showNumber(speed)
+    }
+})
 makerbit.onIrButton(IrButton.Number_9, IrButtonAction.Pressed, function () {
     isGoingForward = false
     maqueenPlusV2.setIndexColor(2, maqueenPlusV2.NeoPixelColors.Orange)
@@ -42,6 +63,8 @@ makerbit.onIrButton(IrButton.Number_9, IrButtonAction.Pressed, function () {
 })
 let distance = 0
 let isGoingForward = false
+let speed = 0
+speed = 150
 makerbit.connectIrReceiver(DigitalPin.P16, IrProtocol.Keyestudio)
 basic.forever(function () {
     distance = maqueenPlusV2.readUltrasonic(DigitalPin.P13, DigitalPin.P14)
